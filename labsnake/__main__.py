ebdbb11af8314a0182f0a00ef9cd8abe40ce10f4
crnbaker <christian.baker@kcl.ac.uk>
2022-09-21 10:31:31 +0100

    Example script displays video and plots colour histogram in separate OpenCV and Matplotlib windows

--- labsnake/__main__.py
+++ labsnake/__main__.py
@@ -9,38 +9,49 @@ logger = logging.getLogger(__name__)
 HIST_BINS = 100
 
 
+def calc_bin_centres(bin_edges):
+    return (bin_edges[:-1] + bin_edges[1:]) / 2
+
+
 def main():
     cap = cv.VideoCapture(0)
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
 
-    fig, ax = plt.subplots(1, 1)
-    line_data_r = plt.plot([])
-    line_data_g = plt.plot([])
-    line_data_b = plt.plot([])
+    fig, hist_ax = plt.subplots(1, 1)
 
     while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        # if frame is read correctly ret is True
-        if not ret:
+        frame_read_successful, frame = cap.read()
+        if not frame_read_successful:
             print("Can't receive frame (stream end?). Exiting ...")
             break
 
-        r, bin_edges_r = np.histogram(frame[:, :, 0], bins=HIST_BINS)
+        # OpenCV colour format is BGR
+        b, bin_edges_b = np.histogram(frame[:, :, 0], bins=HIST_BINS)
         g, bin_edges_g = np.histogram(frame[:, :, 1], bins=HIST_BINS)
-        b, bin_edges_b = np.histogram(frame[:, :, 2], bins=HIST_BINS)
+        r, bin_edges_r = np.histogram(frame[:, :, 2], bins=HIST_BINS)
 
-        line_data_r.set_data(bin_edges_r, r)
-        line_data_g.set_data(bin_edges_g, g)
-        line_data_b.set_data(bin_edges_b, b)
-        plt.pause(0.001)
+        bin_centres_r = calc_bin_centres(bin_edges_r)
+        bin_centres_g = calc_bin_centres(bin_edges_g)
+        bin_centres_b = calc_bin_centres(bin_edges_b)
 
-        # Display the resulting frame
+        # update the plot
+        hist_ax.clear()
+        hist_ax.plot(bin_centres_r, r, 'r')
+        hist_ax.plot(bin_centres_g, g, 'g')
+        hist_ax.plot(bin_centres_b, b, 'b')
+        plt.pause(1e-3)  # this redraws the MATPLOTLIB plot
+
+        # Display the video frame using OpenCV
         cv.imshow('frame', frame)
 
-        if cv.waitKey(1) == ord('q') or not cv.getWindowProperty("frame", cv.WND_PROP_VISIBLE):
+        if (
+                cv.waitKey(1) == ord('q')  # detect keypress with CV window focus.
+                # The wait is required to display the video frame.
+                or not cv.getWindowProperty("frame", cv.WND_PROP_VISIBLE)  # detect CV window close
+                or not plt.fignum_exists(fig.number)  # detect MATPLOTLIB window close
+        ):
             break
 
     # When everything done, release the capture
