11f2a2b25b3573efb6ef5821abe13654dbb5bb06
francois joubert <fxa.joubert@gmail.com>
2022-09-23 11:19:48 +0200

    Refactor the audio_scope plot.

--- labsnake/__main__.py
+++ labsnake/__main__.py
@@ -8,11 +8,12 @@ import soundcard as sc
 logger = logging.getLogger(__name__)
 
 HIST_BINS = 12
+SAMPLE_RATE = 48000
+NUM_AUDIO_SAMPLES = 1024
 
 
 def calc_bin_centres(bin_edges):
     return (bin_edges[:-1] + bin_edges[1:]) / 2
-
 
 def main():
     camera = cv.VideoCapture(0)
@@ -22,11 +23,17 @@ def main():
         exit()
 
     fig, (hist_ax, sound_ax) = plt.subplots(2, 1)
+
+    sound_ax.set_ylim([-1.,1.])
+    sound_ax.set_xlabel('Time (ms)')
+    sound_ax.set_ylabel('Amplitude')
+
     hist_ax.set_xlabel('Pixel intensity')
     hist_ax.set_ylabel('Frequency')
     hist_ax.set_xlim([0, 255])
     hist_ax.set_ylim([0, 1e6])
 
+    # Todo: Use empty values/lists here instead of control flow
     init = True
 
 
@@ -35,8 +42,8 @@ def main():
         if not frame_read_successful:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-        with microphone.recorder(samplerate=48000) as mic:
-            mic_data = mic.record(numframes=1024)
+        with microphone.recorder(samplerate=SAMPLE_RATE) as mic:
+            mic_data = mic.record(numframes=NUM_AUDIO_SAMPLES)
 
         # OpenCV colour format is BGR
         b, bin_edges_b = np.histogram(frame[:, :, 0], bins=HIST_BINS)
@@ -51,8 +58,9 @@ def main():
             r_line, = hist_ax.plot(bin_centres_r, r, 'r')
             g_line, = hist_ax.plot(bin_centres_g, g, 'g')
             b_line, = hist_ax.plot(bin_centres_b, b, 'b')
-            sound_line_l, = sound_ax.plot(mic_data[:,0])
-            sound_line_r, = sound_ax.plot(mic_data[:,1])
+            time_in_ms = 1e3 * np.linspace(0,NUM_AUDIO_SAMPLES/SAMPLE_RATE - 1/NUM_AUDIO_SAMPLES, NUM_AUDIO_SAMPLES)
+            sound_line_l, = sound_ax.plot(time_in_ms, mic_data[:,0])
+            sound_line_r, = sound_ax.plot(time_in_ms, mic_data[:,1])
             init = False
         else:
             r_line.set_data(bin_centres_r, r)
